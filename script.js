37c6eec1a5a4323bc6aadeba61dd91e3f5824da8
Felix Oppermann <felixoppermann@freenet.de>
2024-07-21 14:09:34 +0200

    added table and some logic

---
+++ script.js
@@ -0,0 +1,37 @@
+// script.js
+
+// Initialer Zustand der Felder
+let fields = [
+    null,
+    'circle',
+    'circle',
+    'cross',
+    null,
+    null,
+    'cross',
+    'circle',
+    null,
+];
+
+// Funktion, um die Tabelle zu rendern
+function render() {
+    let content = document.getElementById('content');
+    let tableHtml = '<table>';
+
+    for (let i = 0; i < 3; i++) {
+        tableHtml += '<tr>';
+        for (let j = 0; j < 3; j++) {
+            let index = i * 3 + j;
+            console.log(index)
+            let cellContent = fields[index] === 'circle' ? 'o' : fields[index] === 'cross' ? 'x' : '';
+            tableHtml += `<td>${cellContent}</td>`;
+        }
+        tableHtml += '</tr>';
+    }
+
+    tableHtml += '</table>';
+    content.innerHTML = tableHtml;
+}
+
+// Aufruf der render-Funktion, um die Tabelle darzustellen
+render();
